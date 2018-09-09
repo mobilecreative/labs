4e2bb343532658e04fa6a77c1a6fd9b8eeb2cde2
Javier González <javier.gonzalez@baoss.com>
2018-09-09 16:18:24 +0200

    project setup

---
+++ labsCreativeMobile/labsCreativeMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  labsCreativeMobile
+//
+//  Created by Javier González on 09/09/2018.
+//  Copyright © 2018 Javier Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
